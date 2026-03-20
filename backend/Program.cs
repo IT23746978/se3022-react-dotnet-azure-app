db1a547f830867130dc074d6a3c89b519e47abb7
IT23746978 <it23746978@my.sliit.lk>
2026-03-20 13:50:01 +0530

    Initial commit - React and .NET projects

---
+++ backend/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors("AllowReact");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
